e71fe47a3745ce8a86299d319c9428c030c42ca6
zhuxinyue <904669871@qq.com>
2019-07-04 16:54:49 +0800

    write a new test

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,34 @@
 from selenium import webdriver
+import time
+import unittest
 
-browser=webdriver.Chrome()
-browser.get('http://localhost:8000')
+chrome_driver='C:/Users/xinyue/AppData/Local/Google/Chrome/chromedriver_win32/chromedriver.exe'
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser=webdriver.Chrome(executable_path=chrome_driver)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get("http://localhost:8000")
+        self.assertIn("To-Do",self.browser.title)
+        self.fail("finish the test")
+
+if __name__=='__main__':
+    unittest.main()
+
+
+
+#browser=webdriver.Chrome(executable_path=chrome_driver)
+#time.sleep(5)
+
+#browser.get("http://localhost:8000")
+
+#assert 'To-Do' in browser.title,"Browser title was "+browser.title
+
+
+
+#browser.quit()
