686bb7b4d7cbbb54a2af1b9e73a6ed99cdb9d36f
zhuxinyue <904669871@qq.com>
2019-07-05 17:20:46 +0800

    write a function test check if the set can input a to-do item

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import time
 import unittest
+
 
 chrome_driver='C:/Users/xinyue/AppData/Local/Google/Chrome/chromedriver_win32/chromedriver.exe'
 
@@ -13,8 +15,31 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
+        #打开网站的首页
         self.browser.get("http://localhost:8000")
+
+        #看到该页面的标题为to-do lists
         self.assertIn("To-Do",self.browser.title)
+        heafer_text=self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do',heafer_text)
+
+        #她准备输入一个待办事项
+        inputbox=self.browser.find_elemwnt_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('palceholder'),'enter a to-do item')
+
+        #她在输入区输入buy peacock feathers
+        inputbox.send_keys('buy peacock feathers')
+
+        #点击enter后，页面刷新，待办区出现一条新item
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(5)
+
+        table=self.browser.find_element_by_id('id_list_table')
+        rows=table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text=='1:Buy peacock feathers' for row in rows))
+
+        #仍然存在一个输入框等待她输入待办事项
+
         self.fail("finish the test")
 
 if __name__=='__main__':
