b41ed3833c2dc8c5452eb1d081cecfc2aca9fe6a
zhuxinyue <904669871@qq.com>
2019-07-05 16:11:50 +0800

    basic view now return minimal HTML

--- lists/views.py
+++ lists/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 from . import models
 
 
-def home_page():
-    pass
+def home_page(request):
+    return HttpResponse('<html><title>To-Do lists</title></html>')
