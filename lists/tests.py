9b902c99f1c19ac899893d7573431616cac3899f
zhuxinyue <904669871@qq.com>
2019-07-05 15:45:31 +0800

    finish first function test

--- lists/tests.py
+++ lists/tests.py
@@ -1,11 +1,14 @@
 from django.test import TestCase
+from django.urls import resolve
+from  lists.views import home_page
 import unittest
 
 # Create your tests here.
 class SmokeTest(TestCase):
 
-    def test_bad_maths(self):
-        self.assertEqual(1+1,3)
+    def test_root_url_resolves_to_home_page_view(self):
+        found=resolve('/')
+        self.assertEqual(found.func,home_page)
 
 if __name__=='__main__':
     unittest.main()
