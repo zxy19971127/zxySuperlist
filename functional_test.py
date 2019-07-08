dd6bffe6bbd7f12ed02d44df10406c674f277737
zhuxinyue <904669871@qq.com>
2019-07-08 16:07:42 +0800

    aangetest

--- functional_test.py
+++ functional_test.py
@@ -14,6 +14,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self,row_text):
+        table=self.browser.find_element_by_id('id_list_table')
+        rows=table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text,[row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         #打开网站的首页
         self.browser.get("http://localhost:8000")
@@ -33,7 +38,8 @@ class NewVisitorTest(unittest.TestCase):
 
         #点击enter后，页面刷新，待办区出现一条新item
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(5)
+        time.sleep(1)
+        self.check_for_row_in_list_table('1: buy peacock feather')
 
         table=self.browser.find_element_by_id('id_list_table')
         rows=table.find_elements_by_tag_name('tr')
@@ -50,7 +56,10 @@ class NewVisitorTest(unittest.TestCase):
 
         #点击enter后，页面刷新，待办区出现一条新item
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(5)
+        time.sleep(1)
+
+        self.check_for_row_in_list_table('1: buy peacock feather')
+        self.check_for_row_in_list_table('2: use peacock feathers to make a fly')
 
         table=self.browser.find_element_by_id('id_list_table')
         rows=table.find_elements_by_tag_name('tr')
--- lists/tests.py
+++ lists/tests.py
@@ -7,7 +7,6 @@ import unittest
 
 # Create your tests here.
 class SmokeTest(TestCase):
-
     def test_root_url_resolves_to_home_page_view(self):
         found=resolve('/')
         self.assertEqual(found.func,home_page)
