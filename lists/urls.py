4e62ea41f99bf446a51b0c06d5fa75669e7673b3
zhuxinyue <904669871@qq.com>
2019-07-29 12:05:41 +0800

    change the url files

--- lists/urls.py
+++ lists/urls.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from django.urls import path,re_path
+from django.conf.urls import url
 from . import views
 
+urlpatterns = [
+
+    url(r'^new$',views.new_list,name='new_list'),
+    url(r'^(\d+)/$',views.view_list,name='view_list'),
+    url(r'^(\d+)/add_item$',views.add_item,name='add_item')
+]
--- superlists/urls.py
+++ superlists/urls.py
@@ -17,12 +17,11 @@ from django.contrib import admin
 from django.urls import path,re_path
 from django.urls import include
 from django.conf.urls import url
-from lists import views
+from lists import views as list_views
+from lists import urls as list_urls
 
 urlpatterns = [
     #url('admin/', admin.site.urls),
-    url(r'^$',views.home_page,name='home'),
-    url(r'^lists/new$',views.new_list,name='new_list'),
-    url(r'^lists/(\d+)/$',views.view_list,name='view_list'),
-    url(r'^lists/(\d+)/add_item$',views.add_item,name='add_item')
+    url(r'^$',list_views.home_page,name='home'),
+    url(r'^lists/',include(list_urls)),
 ]
